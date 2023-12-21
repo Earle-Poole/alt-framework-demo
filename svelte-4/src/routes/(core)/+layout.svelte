cd599d3c07d00a29080a586f0a1191122e169401
Earle Poole <eepoole4@gmail.com>
2023-12-21 15:28:55 -0600

    Re-instantiate svelte

---
+++ svelte-4/src/routes/(core)/+layout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  // props passed from the load function
+  export let title;
+
+  console.log('title: ', $title)
+</script>
+
+<!-- <Header /> -->
+Header!!!
+<main class="flex justify-center">
+  <section class="w-full max-w-screen-xl">
+    <slot />
+  </section>
+</main>
+Footer!!!
